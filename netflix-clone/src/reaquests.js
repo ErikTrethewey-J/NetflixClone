54f8f3854495484284a2a60cd8fca155908f396e
ErikTrethewey-J <erikenobi@gmail.com>
2020-11-11 15:22:06 -0800

    Finished setting up exporting reqeusts from data for movie info.

--- netflix-clone/src/reaquests.js
+++ netflix-clone/src/reaquests.js
@@ -1,1 +1,14 @@
-const APIKEY = "3aa1a5d54afcacaeb63346e148961b2f";
+const API_KEY = "3aa1a5d54afcacaeb63346e148961b2f";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_geners=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_geners=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_geners=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_geners=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_geners=99`    
+}
+
+export default requests;
