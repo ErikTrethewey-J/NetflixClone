314eada1e1715192f554a50739848ed4837fdb1c
ErikTrethewey-J <erikenobi@gmail.com>
2020-11-11 16:54:58 -0800

    Fixed bug issue. Was not importing axios correctly. Currently fixed.

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
+  console.log(requests.fetchNetflixOriginals)
   return (
     <div className="App">
       <h1>Create a Netflix Clone!</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now"/>
-      
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import requests from './requests';
 
-function Row({ title }) {
-    const [movie, setMovies] = useState([])
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([])
+    console.log(requests.fetchNetflixOriginals)
+    console.log(fetchUrl)
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        //if [], run once when the row loads, and dont run again
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request)
+            // setMovies(request.data.results)
+            return request;
+        }
+        fetchData();
+    }, [])
 
-    
 
     return (
         <div>
