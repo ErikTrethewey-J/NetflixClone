cafe8e18bbb05b2a8e7dbcdf5075c70b6d8a36ae
ErikTrethewey-J <erikenobi@gmail.com>
2020-11-11 17:38:13 -0800

    Added CSS to app.

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -3,6 +3,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([])
@@ -13,18 +16,28 @@ function Row({ title, fetchUrl }) {
         //if [], run once when the row loads, and dont run again
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request)
-            // setMovies(request.data.results)
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, [])
+    }, [fetchUrl])
+
+    console.log(movies)
 
 
     return (
-        <div>
+        <div className="row">
             <h2>{ title }</h2>
-            
+            <div className="row__posters">
+                {/* {several row__poster(s)} */}
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className="row__poster"  
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name}
+                    />
+                ))}
+            </div>
             {/* {container -> posters} */}
         </div>
     )
